e52da1ab77909266180c12d32596fd1ff5cb9a86
Tarun Jagadish <tarunjagadish28@gmail.com>
2024-11-25 15:22:37 -0800

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -9,45 +9,62 @@ class NoSQLTestCases:
         self.schema_description=data_explore(f'NoSQL/{db_name}', print_schema=False)
         self.max_sample = max_sample
 
-    #creating options for multiple databases
-    if self.db_name == 'formula_1':
-        self.t = [
-    # constructorStandings
-    ("constructorStandings", "Fetch the minimum points from constructorStandings, grouped by constructorStandingsId."),
-    ("constructorStandings", "Get the minimum position from constructorStandings where points = 7."),
-    ("constructorStandings", "Get the mean of points from constructorStandings."),
-    ("constructorStandings", "Fetch the total points from constructorStandings."),
-    ("constructorStandings", "Get the summation of points from constructorStandings, that are grouped by _id."),
-    
-    # circuits
-    ("circuits", "Fetch the minimum long from circuits, ordered by lat."),
-    ("circuits", "Fetch the variance in long from circuits where circuitId > 50"),
-    
-    # constructors
-    ("constructors", "Fetch _id from constructors."),
-    
-    # races
-    ("races", "Get year from races."),
-    ("races", "Fetch round from races.")
-]
-    elif self.db_name == 'debit_card_specialization':
-        self.t = [
-            #customers
-            ("customers","Fetch unique values of CustomerID from customers where Segment = 'SME'."),
-            ("customers", "Fetch unique values of CustomerID from customers where Currency = 'EUR'."),
+        #creating options for multiple databases
+        if self.db_name == 'formula_1':
+            self.t = [
+        # constructorStandings
+        ("constructorStandings", "Fetch the minimum points from constructorStandings, grouped by constructorStandingsId."),
+        ("constructorStandings", "Get the minimum position from constructorStandings where points = 7."),
+        ("constructorStandings", "Get the mean of points from constructorStandings."),
+        ("constructorStandings", "Fetch the total points from constructorStandings."),
+        ("constructorStandings", "Get the summation of points from constructorStandings, that are grouped by _id."),
+        
+        # circuits
+        ("circuits", "Fetch the minimum long from circuits, ordered by lat."),
+        ("circuits", "Fetch the variance in long from circuits where circuitId > 50"),
+        
+        # constructors
+        ("constructors", "Fetch _id from constructors."),
+        
+        # races
+        ("races", "Get year from races."),
+        ("races", "Fetch round from races.")
+    ]
+        elif self.db_name == 'debit_card_specialization':
+            self.t = [
+                #customers
+                ("customers","Fetch unique values of CustomerID from customers where Segment = 'SME'."),
+                ("customers", "Fetch unique values of CustomerID from customers where Currency = 'EUR'."),
 
-            #gasstations
-            ("gasstations","Fetch unique values of ChainID from gasstations where Segment = 'Premium'."),
-            ("gasstations","Get unique values of GasStationID from gasstations where Country = 'CZE'."),
+                #gasstations
+                ("gasstations","Fetch unique values of ChainID from gasstations where Segment = 'Premium'."),
+                ("gasstations","Get unique values of GasStationID from gasstations where Country = 'CZE'."),
 
-            #products
-            ("products","Get Description from products where ProductID = 5."),
+                #products
+                ("products","Get Description from products where ProductID = 5."),
 
-            #transactions_1k
-            ("transactions_1k","Fetch the minimum Price from transactions_1k."),
-            ("transactions_1k","Fetch the variance in Amount from transactions_1k where Price >100."),
-            ("transactions_1k","Get CardID from transactions_1k.")
-        ]
+                #transactions_1k
+                ("transactions_1k","Fetch the minimum Price from transactions_1k."),
+                ("transactions_1k","Fetch the variance in Amount from transactions_1k where Price >100."),
+                ("transactions_1k","Get CardID from transactions_1k.")
+            ]
+        elif self.db_name == 'california_schools':
+            self.t = [
+                ("satscores", "Get the AvgScrMath where cds = '1100170000000' from Schools."),
+                ("satscores", "Retrieve the NumTstTakr where sname = 'FAME Public Charter' in Schools."),
+                ("satscores", "Fetch unique cds values from Schools."),
+                ("satscores", "Fetch distinct rtype where AvgScrRead > 500 in Schools."),
+
+                ("frpm", "Get the School Name where CDSCode = '01100170109835' from Schools."),
+                ("frpm", "Retrieve the Percent (%) Eligible FRPM (Ages 5-17) where School Name = 'Envision Academy for Arts & Technology' in Schools."),
+                ("frpm", "Fetch unique Charter School Numbers from Schools."),
+                ("frpm", "Fetch distinct School Type where Percent (%) Eligible Free (K-12) > 0.5 in Schools."),
+
+                ("schools", "Get the AdmEmail1 where CDSCode = '01100170000000' from Schools."),
+                ("schools", "Retrieve the CharterNum where Charter = 1 in Schools."),
+                ("schools", "Fetch unique District values from Schools."),
+                ("schools", "Fetch distinct StatusType where State = 'CA' in Schools."),
+            ]
         
     def __call__(self, t):
         print("#"*100)
@@ -84,43 +101,60 @@ class SQLTestCases:
         self.db_name = db_name
         self.schema_description=data_explore(f'SQL/{db_name}', print_schema=False)
         self.max_sample = max_sample
-    if self.db_name == 'thrombosis_prediction':
-        self.t = [
-    # Examination table
-    ("Examination", "Get the average of ANA where Thrombosis = 1 from Examination."),
-    ("Examination", "Retrieve the sum of ANA where Thrombosis = 2 in Examination."),
-    ("Examination", "Fetch unique values of Symptoms where Thrombosis = 3 from Examination."),
-    ("Examination", "Fetch distinct Diagnosis values where Thrombosis = 2 in Examination."),
+        if self.db_name == 'thrombosis_prediction':
+            self.t = [
+        # Examination table
+        ("Examination", "Get the average of ANA where Thrombosis = 1 from Examination."),
+        ("Examination", "Retrieve the sum of ANA where Thrombosis = 2 in Examination."),
+        ("Examination", "Fetch unique values of Symptoms where Thrombosis = 3 from Examination."),
+        ("Examination", "Fetch distinct Diagnosis values where Thrombosis = 2 in Examination."),
 
 
-    # Laboratory table
-    ("Laboratory", "Fetch the maximum value of GOT where GOT > 100 from Laboratory."),
-    ("Laboratory", "Get the average of CPK where TG > 100 from Laboratory."),
-    ("Laboratory", "Retrieve the average value of ALB where HGB > 12 from Laboratory."),
-    ("Laboratory", "Get the total count of RBC where T-BIL > 0.2 in Laboratory."),
-    ("Laboratory", "Fetch the range of UA values where CRP = 2 in Laboratory."),
-    ("Laboratory", "Retrieve the minimum value of ALP where ALP > 15 in Laboratory."),
+        # Laboratory table
+        ("Laboratory", "Fetch the maximum value of GOT where GOT > 100 from Laboratory."),
+        ("Laboratory", "Get the average of CPK where TG > 100 from Laboratory."),
+        ("Laboratory", "Retrieve the average value of ALB where HGB > 12 from Laboratory."),
+        ("Laboratory", "Get the total count of RBC where T-BIL > 0.2 in Laboratory."),
+        ("Laboratory", "Fetch the range of UA values where CRP = 2 in Laboratory."),
+        ("Laboratory", "Retrieve the minimum value of ALP where ALP > 15 in Laboratory."),
 
 
-    # Patient table
-    ("Patient", "Fetch the total count of PatientID where SEX = 1 from Patient."),
-    ("Patient", "Get all unique values of SEX where ID = 4060811 from Patient."),
-    ("Patient", "Fetch all non-null Description values where PatientID = 1124385 from Patient.")
+        # Patient table
+        ("Patient", "Fetch the total count of PatientID where SEX = 1 from Patient."),
+        ("Patient", "Get all unique values of SEX where ID = 4060811 from Patient."),
+        ("Patient", "Fetch all non-null Description values where PatientID = 1124385 from Patient.")
 
-]
-    elif self.db_name == 'superhero':
-        self.t = [
+    ]
+        elif self.db_name == 'superhero':
+            self.t = [
 
-            #superhero table
-            ("superhero","Fetch superhero_name values from superhero where full_name = 'Charles Chandler'."),
-            ("superhero", "Get all unique values of full_name where gender_id = 1 from superhero"),
-            ("superhero", "Fetch superhero_name values where heigh_cm > 190 from superhero."),
+                #superhero table
+                ("superhero","Fetch superhero_name values from superhero where full_name = 'Charles Chandler'."),
+                ("superhero", "Get all unique values of full_name where gender_id = 1 from superhero"),
+                ("superhero", "Fetch superhero_name values where heigh_cm > 190 from superhero."),
 
-            #race table
-            
+                #race table
+                
 
-            
-        ]
+                
+            ]
+        elif self.db_name == 'european_football_2':
+            self.t = [
+                ("Leagues", "Get the total count of leagues where country_id > 10000 from Leagues."),
+                ("Leagues", "Fetch unique country_ids from Leagues."),
+                ("Leagues", "Fetch distinct league names where country_id = 7809 in Leagues."),
+                ("Leagues", "Retrieve the id of the league where country_id = 1729 in Leagues."),
+
+                ("Players", "Get the player_name where id = 1 from Players."),
+                ("Players", "Retrieve the height of the player where player_name = 'Aaron Cresswell' in Players."),
+                ("Players", "Fetch unique player_fifa_api_id values from Players."),
+                ("Players", "Fetch distinct birthdays where weight = 187 in Players."),
+
+                ("Teams", "Get the team_long_name where id = 1 from Teams."),
+                ("Teams", "Retrieve the team_short_name where team_fifa_api_id = 675 in Teams."),
+                ("Teams", "Fetch unique team_api_id values from Teams."),
+                ("Teams", "Fetch distinct team_fifa_api_id where team_long_name = 'Beerschot AC' in Teams.")
+            ]
    
     def __call__(self, t):
         print("#"*100)
@@ -145,5 +179,3 @@ class SQLTestCases:
         except:
             print("Results : ", [])
             return []
-
-
